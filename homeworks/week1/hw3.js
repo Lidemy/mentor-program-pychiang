488eee73cbf0deec84436cd4b1038e117ec8aac4
Peggy Chiang <cljh80018@gmail.com>
2018-02-02 18:45:44 -0800

    Submit week1 hw1,2,3,6

--- homeworks/week1/hw3.js
+++ homeworks/week1/hw3.js
@@ -1,3 +1,13 @@
 export const isPrime = (n) => {
-  return false;
+    //1 is not a prime number
+    if (n === 1) {  
+        return false; 
+    }
+    //test if the number can be divided by any numbers smaller than itself
+    for (var i = 2; i < n; i++) {
+        if (n % i === 0) {  
+            return false;
+        }
+    }
+  return true;
 }
